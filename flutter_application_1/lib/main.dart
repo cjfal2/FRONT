915b4bf8db2f996b94d29f17b59518635778176f
cjfal2 <cjfal2@kakao.com>
2023-03-08 00:04:57 +0900

    0307 flutter 공부 4강까지

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -16,22 +16,55 @@ class MyApp extends StatelessWidget {
         //   child: Container(width: 50, height: 50, color: Colors.blue,)
         // )
         home: Scaffold(
-            appBar: AppBar(
-              title: Text("앱바"),
-              backgroundColor: Color.fromARGB(255, 79, 202, 255),
-            ),
-            body: Text("바디"),
-            bottomNavigationBar: BottomAppBar(
-                height: 70,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Icon(Icons.phone),
-                    Icon(Icons.message),
-                    Icon(Icons.contact_page),
-                  ]
-                ),
-            
-            )));
+      appBar: AppBar(
+        title: Text("앱바"),
+        leading: Icon(Icons.api),
+        actions: [Icon(Icons.star), Icon(Icons.message)], // 우측 아이콘들
+        backgroundColor: Color.fromARGB(255, 79, 202, 255),
+      ),
+      body: SizedBox(
+          child: IconButton(
+        onPressed: () {},
+        icon: Icon(Icons.adb_sharp, color: Color.fromARGB(255, 249, 110, 110)),
+      )),
+    ));
   }
 }
+      // body: Align(
+      //   alignment: Alignment.topCenter,
+      //   child: Container(
+      //       width: 50,
+      //       height: 50,
+      
+      //       // margin: EdgeInsets.all(20), // 마진주기(4방향다)
+      //       margin: EdgeInsets.fromLTRB(0, 5, 10, 20), // 마진주기(4방향 각각)
+      //       padding: EdgeInsets.all(20), // 패딩주기
+      //       // 자잘한 디자인들은 decoration 이용
+      //       decoration: BoxDecoration(
+      //         border: Border.all(color: Colors.black),
+      //         color: Colors.brown
+      //       ),
+      //       child: Text("패딩 테스트")),
+      // ),
+
+        // home: Scaffold(
+        //     appBar: AppBar(
+        //       title: Text("앱바"),
+        //       backgroundColor: Color.fromARGB(255, 79, 202, 255),
+        //     ),
+        //     body: Text("바디"),
+        //     bottomNavigationBar: BottomAppBar(
+        //         // ignore: avoid_unnecessary_containers
+        //         child: SizedBox(
+        //           height: 70,
+        //           child: Row(
+        //             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        //             children: [
+        //               Icon(Icons.phone),
+        //               Icon(Icons.message),
+        //               Icon(Icons.contact_page),
+        //             ]
+        //           ),
+        //         ),
+            
+        //     )));

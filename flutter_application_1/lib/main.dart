6e624c53a643efb00fb1184e10dc995c8348e3dc
cjfal2 <cjfal2@kakao.com>
2023-03-08 23:15:26 +0900

    0308 집커밋

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_unnecessary_containers
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,49 +12,113 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        // 여기다 코드짜기
-        // home: Image.asset('dotori.jpg')
-        // home: Center(
-        //   child: Container(width: 50, height: 50, color: Colors.blue,)
-        // )
         home: Scaffold(
-      appBar: AppBar(
-        title:
-            Text("역삼동", style: TextStyle(color: Color.fromARGB(255, 0, 0, 0))),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.search, color: Color.fromARGB(255, 0, 0, 0)),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.menu, color: Color.fromARGB(255, 0, 0, 0)),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon:
-                Icon(Icons.notifications, color: Color.fromARGB(255, 0, 0, 0)),
-          ),
-        ],
-        backgroundColor: Color.fromARGB(255, 238, 238, 238),
-      ),
-      body: Row(
-        children: [
-          Flexible(
-              flex: 3,
-              child: Container(
-                color: Color.fromRGBO(255, 61, 61, 0),
-              )),
-          Flexible(
-              flex: 7,
-              child: Container(
-                color: Color.fromRGBO(42, 223, 255, 0),
-              )),
-        ],
-      ),
-    ));
+            appBar: AppBar(),
+            body: ListView(
+              // controller: ScrollController(), // 스크롤 위치 감시
+              children: [
+                CostumWidget(),
+                CostumWidget(),
+                CostumWidget(),
+                CostumWidget(),
+                CostumWidget(),
+                CostumWidget(),
+                CostumWidget(),
+              ],
+            )));
   }
 }
+
+// 커스텀 위젯
+class CostumWidget extends StatelessWidget {
+  const CostumWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 300,
+      child: Text('안녕1'),
+    );
+  }
+}
+
+// 또 다른 커스텀
+var a = SizedBox(
+  child: Text("안녕2"),
+);
+
+
+
+
+
+
+
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         home: Scaffold(
+//       appBar: AppBar(
+//         title:
+//             Text("역삼동", style: TextStyle(color: Color.fromARGB(255, 0, 0, 0))),
+//         actions: [
+//           IconButton(
+//             onPressed: () {},
+//             icon: Icon(Icons.search, color: Color.fromARGB(255, 0, 0, 0)),
+//           ),
+//           IconButton(
+//             onPressed: () {},
+//             icon: Icon(Icons.menu, color: Color.fromARGB(255, 0, 0, 0)),
+//           ),
+//           IconButton(
+//             onPressed: () {},
+//             icon:
+//                 Icon(Icons.notifications, color: Color.fromARGB(255, 0, 0, 0)),
+//           ),
+//         ],
+//         backgroundColor: Color.fromARGB(255, 238, 238, 238),
+//       ),
+//       body: Container(
+//         height: 150,
+//         padding: EdgeInsets.all(10),
+//         child: Row(
+//           children: [
+//             Image.asset("dotori.jpg", width: 130, height: 130,),
+//             SizedBox(
+//               width: 300,
+//               child: Column(
+//                 crossAxisAlignment: CrossAxisAlignment.start,
+//                 children: [
+//                   Text("도토리팝니다"),
+//                   Text("수원어딘가"),
+//                   Text("10억원"),
+//                   Row(
+//                     mainAxisAlignment: MainAxisAlignment.end,
+//                     children: [
+//                       Icon(Icons.favorite),
+//                       Text("4")
+//                     ],)
+//                 ],
+//               ),
+//             )
+//           ],
+//         ),
+
+//       )
+//     ));
+//   }
+// }
+
+
+
+
+
+
+
+
       // body: Align(
       //   alignment: Alignment.topCenter,
       //   child: Container(
@@ -69,6 +135,12 @@ class MyApp extends StatelessWidget {
       //       ),
       //       child: Text("패딩 테스트")),
       // ),
+
+
+
+
+
+
 
         // home: Scaffold(
         //     appBar: AppBar(

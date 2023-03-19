64785491f8ee0780414cf1a4124bc3f5d60fdc27
cjfal2 <cjfal2@kakao.com>
2023-03-20 00:29:36 +0900

    0319 nomad ing

---
+++ nomad_toonflix/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:nomad_toonflix/screens/home_screens.dart';
+import 'package:nomad_toonflix/services/api_service.dart';
+
+void main() {
+  ApiService().getTodaysToons();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const HomeScreen(),
+    );
+  }
+}

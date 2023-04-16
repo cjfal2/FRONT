d7e5888317f6213f4c3860d11d88d2aaf99b5e31
cjfal2 <cjfal2@kakao.com>
2023-04-17 00:26:18 +0900

    노마드 RN 웨더앱 95% 완성

--- nomadweather/App.js
+++ nomadweather/App.js
@@ -1,15 +1,52 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, ScrollView, Dimensions } from "react-native";
+import * as Location from "expo-location";
+import { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Dimensions,
+  ActivityIndicator,
+} from "react-native";
 
 // 화면의 크기를 가져오는 코드
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
+const API_KEY = "";
 
 export default function App() {
+  const [city, setCity] = useState("Loading...");
+  const [days, setDays] = useState([]);
+  const [ok, setOk] = useState(true);
+  const getWeather = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    // granted가 true여야 확인이 가능하다
+    if (!granted) {
+      setOk(false);
+    }
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false }
+    );
+    setCity(location[0].district);
+    const response = await fetch(
+      `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    );
+    const json = await response.json();
+    setDays(json.daily);
+  };
+  useEffect(() => {
+    getWeather();
+  }, []);
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto"></StatusBar>
       <View style={styles.city}>
-        <Text style={styles.cityName}>Seoul</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
       {/* 스크롤뷰에는 contentContainerStyle를 사용해야 한다. */}
       <ScrollView
@@ -18,22 +55,23 @@ export default function App() {
         pagingEnabled
         showsHorizontalScrollIndicator={false}
       >
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
+        {days.length === 0 ? (
+          <View style={styles.day}>
+            <ActivityIndicator
+              color={"white"}
+              size={"large"}
+              style={{ marginTop: 10 }}
+            />
+          </View>
+        ) : (
+          days.map((day, index) =>
+          
+          <View key={index} style={styles.day}>
+            <Text style={styles.temp}>{parseFloat(day.temp.day).toFixed(1)}</Text>
+            <Text style={styles.main}>{day.weather[0].main}</Text>
+            <Text style={styles.description}>{day.weather[0].description}</Text>
+          </View>)
+        )}
       </ScrollView>
     </View>
   );
@@ -42,7 +80,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "tomato",
+    backgroundColor: "#96ec40",
   },
   city: {
     flex: 1.2,
@@ -59,11 +97,15 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   temp: {
-    fontSize: 170,
+    fontSize: 140,
+    fontWeight: "600",
     marginTop: 50,
   },
-  description: {
+  main: {
     marginTop: -30,
-    fontSize: 60,
+    fontSize: 50,
+  },
+  description: {
+    fontSize: 20,
   },
 });
